4296996f507f832f749eaceb03d2c64f7a5fff41
Marco Binda <marcobinda@yahoo.com>
2023-10-08 15:05:25 -0300

    Add project files.

---
+++ MF-Dev-Backend-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MF_Dev_Backend_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
