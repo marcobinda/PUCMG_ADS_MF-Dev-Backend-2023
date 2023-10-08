97ff48e1e8c10412701d77f6266c41ccc1a7fc52
Marco Binda <marcobinda@yahoo.com>
2023-10-08 18:31:16 -0300

    Configuração do banco de dados e da tabela veículo

---
+++ MF-Dev-Backend-2023/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MF_Dev_Backend_2023.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ MF-Dev-Backend-2023/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MF_Dev_Backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo nome é obrigatório!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo placa é obrigatório!")]
+        public string Placa { get; set; }
+        [Required(ErrorMessage = "O campo ano de fabricação é obrigatório!")]
+        public int AnoFabricacao { get; set; }
+        [Required(ErrorMessage = "O campo ano do modelo é obrigatório!")]
+        public int AnoModelo { get; set; }
+    }
+}
