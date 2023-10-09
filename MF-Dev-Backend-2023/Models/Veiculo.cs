ebb29f4a437fc07d9ab078494f1db2d4a7fa2144
Marco Binda <marcobinda@yahoo.com>
2023-10-09 11:38:58 -0300

    CRUD Veículo

--- MF-Dev-Backend-2023/Models/Veiculo.cs
+++ MF-Dev-Backend-2023/Models/Veiculo.cs
@@ -13,8 +13,10 @@ namespace MF_Dev_Backend_2023.Models
         [Required(ErrorMessage = "O campo placa é obrigatório!")]
         public string Placa { get; set; }
         [Required(ErrorMessage = "O campo ano de fabricação é obrigatório!")]
+        [Display(Name ="Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
         [Required(ErrorMessage = "O campo ano do modelo é obrigatório!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
